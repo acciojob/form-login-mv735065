6e40c63811112d59343722dea1cf90dce9140129
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-10-30 18:15:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	let inputs=document.querySelectorAll("input");
 
+	let fname=inputs[0].value;
+	let lname=inputs[1].value;
+	
+alert(fname+" "+lname);
 }
+
+
